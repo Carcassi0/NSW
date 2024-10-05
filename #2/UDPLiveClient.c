8da17e6a43591ec1da7d2ead4ba73e84bed3ab62
WEIUIJONG <dnldmlwhd@ajou.ac.kr>
2024-10-06 00:23:40 +0900

    modified:   #2/UDPLiveClient.c 	modified:   #2/UDPLiveServer.c 	modified:   .DS_Store

--- #2/UDPLiveClient.c
+++ #2/UDPLiveClient.c
@@ -2,56 +2,90 @@
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
-#include <unistd.h>      // for close()
-#include <arpa/inet.h>   // for inet_addr() and htons()
-#include <sys/socket.h>  // for socket functions
-#include <netinet/in.h>  // for sockaddr_in
+#include <unistd.h>     // for close()
+#include <arpa/inet.h>  // for inet_addr() and htons()
+#include <sys/socket.h> // for socket functions
+#include <netinet/in.h> // for sockaddr_in
 
 #define BUFSIZE 512
 
+char buf[BUFSIZE];
+char nickName[BUFSIZE];
+char sendBuf[BUFSIZE];
+int len;
+struct sockaddr_in peeraddr;
+socklen_t addrlen;
+int server_port = 0;
+char server_ip[INET_ADDRSTRLEN];
+
 void *ThreadMain(void *arg); // Main program of a thread
 void err_quit(const char *msg);
+void *sendMessage(void *arg);
+void *rcvMessage(void *arg);
 
-struct ThreadArgs {
-  int clntSock; // Socket descriptor for client
+struct ThreadArgs
+{
+    int clntSock; // Socket descriptor for client
 };
 
-int main(int argc, char *argv[]){
-
+int main(int argc, char *argv[])
+{
 
     int retval;
-    struct sockaddr_in peeraddr;
-    socklen_t addrlen;
     // 클라이언트 정보
     // 닉네임, 메시지 => 채팅창 출력(전송) 형식: "[닉네임] 채팅 메시지"
     // UDP 서버는 클라이언트가 보낸 닉네임을 바탕으로 채팅 참여자 정보 수집
-    
 
+    // IP 주소와 포트 번호 그리고 채팅 닉네임 입력 받기
+    // char server_ip[INET_ADDRSTRLEN];  // IPv4 주소의 최대 길이로 설정
+    // int server_port = 0;
 
-    // IP 주소와 포트 번호 입력 받기
-    char server_ip[INET_ADDRSTRLEN];  // IPv4 주소의 최대 길이로 설정
-    int server_port = 0;
+    // Server address initialize
+    // struct sockaddr_in serveraddr;
+    // memset(&serveraddr, 0, sizeof(serveraddr));
+    // serveraddr.sin_family = AF_INET;
+    // serveraddr.sin_port = htons(server_port);
+    // serveraddr.sin_addr.s_addr = inet_addr(server_ip);
 
-    printf("Enter server IP address: ");
-    fgets(server_ip, sizeof(server_ip), stdin);
-    server_ip[strcspn(server_ip, "\n")] = 0;  // 개행 문자 제거
+    for (;;)
+    {
+        struct ThreadArgs *threadArgs = (struct ThreadArgs *)malloc(sizeof(struct ThreadArgs));
 
-    printf("Enter server port number: ");
-    scanf("%d", &server_port); // server default setting: 9000
+        pthread_t sendThread;
+        pthread_t recvThread; // Create client thread for send
+        int sendReturn = pthread_create(&sendThread, NULL, sendMessage, threadArgs);
+        int recvReturn = pthread_create(&recvThread, NULL, rcvMessage, threadArgs);
 
-    // Server port 입력 후 입력 버퍼 클리어
-    while (getchar() != '\n'); // 남아있는 모든 문자를 읽어버리면서 버퍼를 클리어
+        free(threadArgs);
 
-
-    if (server_port <= 0 || server_port > 65535) {
-        printf("Invalid port number.\n");
-        return -1;
     }
+    return 0;
+}
+
+void *ThreadMain(void *threadArgs)
+{
+    // Guarantees that thread resources are deallocated upon return
+    pthread_detach(pthread_self());
+
+    // Extract socket file descriptor from argument
+    int clntSock = ((struct ThreadArgs *)threadArgs)->clntSock;
+    free(threadArgs); // Deallocate memory for argument
+
+    HandleTCPClient(clntSock);
+
+    return (NULL);
+}
+
+void *sendMessage(void *arg)
+{ // 발신 스레드
+    pthread_detach(pthread_self());
 
     int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    int send;
 
-    if(sock < 0){
-         err_quit("socket()");
+    if (sock < 0)
+    {
+        err_quit("socket()");
     }
 
     // Server address initialize
@@ -61,48 +95,39 @@ int main(int argc, char *argv[]){
     serveraddr.sin_port = htons(server_port);
     serveraddr.sin_addr.s_addr = inet_addr(server_ip);
 
-    char *servPort = argv[1]; // First arg:  local port
+    printf("Enter server IP address: ");
+    fgets(server_ip, sizeof(server_ip), stdin);
+    server_ip[strcspn(server_ip, "\n")] = 0; // 개행 문자 제거
 
-    int servSock = SetupTCPServerSocket(servPort); 
+    printf("Enter server port number: ");
+    scanf("%d", &server_port); // server default setting: 9000
+    while (getchar() != '\n')
+        ; // 입력 버퍼 클리어
 
-    for(;;){
-        int clntSock = AcceptTCPConnection(servSock);
-
-        struct ThreadArgs *threadArgs = (struct ThreadArgs *) malloc(
-        sizeof(struct ThreadArgs));
-
-        threadArgs->clntSock = clntSock;
-
-        pthread_t sendThread; // Create client thread for send
-        int sendReturn = pthread_create(&sendThread, NULL, sendMessage, threadArgs); // ThreadMain을 send 스레드 함수로 바꾸기
-
+    if (server_port <= 0 || server_port > 65535)
+    {
+        printf("Invalid port number.\n");
+        return -1;
     }
-    return 0;
-}
 
+    while (1)
+    {
+        // 사용자 닉네임 받기
+        printf("Enter NickName: ");
+        scanf("%s", &nickName);
 
-void *ThreadMain(void *threadArgs) {
-  // Guarantees that thread resources are deallocated upon return
-  pthread_detach(pthread_self());
+        len = strlen(nickName);
+        if (buf[len - 1] == '\n')
+            buf[len - 1] = '\0';
+        if (strlen(buf) == 0)
+            break;
 
-  // Extract socket file descriptor from argument
-  int clntSock = ((struct ThreadArgs *) threadArgs)->clntSock;
-  free(threadArgs); // Deallocate memory for argument
+        while (getchar() != '\n')
+            ; // 입력 버퍼 클리어
 
-  HandleTCPClient(clntSock);
-
-  return (NULL);
-}
-
-void *sendMessage(void *arg){ // 발신 스레드
-    pthread_detach(pthread_self());
-
-    char buf[BUFSIZE + 1];
-    int len;
-
-    while(1){
+        // 메시지 입력
         printf("\n[enter message] ");
-        if (fgets(buf, BUFSIZE + 1, stdin) == NULL)
+        if (fgets(buf, BUFSIZE, stdin) == NULL)
             break;
 
         len = strlen(buf);
@@ -110,17 +135,41 @@ void *sendMessage(void *arg){ // 발신 스레드
             buf[len - 1] = '\0';
         if (strlen(buf) == 0)
             break;
+
+        sprintf(sendBuf, "[%s] %s\n", nickName, buf);
+
+        send = sendto(sock, sendBuf, sizeof(sendBuf), 0, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
+
+        memset(buf, 0, sizeof(buf));
+        memset(sendBuf, 0, sizeof(sendBuf));
     }
-    
-
 }
 
-void *rcvMessage(void *arg){ // 수신 스레드
+void *rcvMessage(void *arg)
+{ // 수신 스레드
     pthread_detach(pthread_self());
+    int recv;
+
+    while (1)
+    {
+        int sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+        addrlen = sizeof(peeraddr);
+
+        recv = recvfrom(sock, buf, BUFSIZE, 0, (struct sockaddr *)&peeraddr, &addrlen);
+
+        // 수신된 메시지 출력
+        buf[recv] = '\0';
+        printf("[Received message] %s\n", buf);
+        memset(buf, 0, sizeof(buf));
+    }
 }
 
-
-void err_quit(const char *msg) {
-    perror(msg);  // 시스템 오류 메시지 출력
-    exit(-1);     // 프로그램 종료
+void err_quit(const char *msg)
+{
+    perror(msg); // 시스템 오류 메시지 출력
+    exit(-1);    // 프로그램 종료
 }
+
+// gcc -o server UDPLiveClient.c
+// ./client
--- #2/UDPLiveServer.c
+++ #2/UDPLiveServer.c
@@ -2,60 +2,178 @@
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
-#include <unistd.h>      // for close()
-#include <arpa/inet.h>   // for inet_addr() and htons()
-#include <sys/socket.h>  // for socket functions
-#include <netinet/in.h>  // for sockaddr_in
+#include <unistd.h>     // for close()
+#include <arpa/inet.h>  // for inet_addr() and htons()
+#include <sys/socket.h> // for socket functions
+#include <netinet/in.h> // for sockaddr_in
+
+#define BUFSIZE 512
+
+char mainBuf[BUFSIZE];
+char nickNameBuf[BUFSIZE];
+char sendBuf[BUFSIZE];
+struct userData userList[BUFSIZE];
+int userCount = 0;
+int len;
+
+// 통계 변수
+int messageNumber = 0;
+
+struct sockaddr_in clientaddr;
+struct sockaddr_in serveraddr;
+
+// memset(&serveraddr, 0, sizeof(serveraddr));
+// serveraddr.sin_family = AF_INET;
+// serveraddr.sin_port = htons(9000);
+// serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
 void *ThreadMain(void *arg); // Main program of a thread
 void err_quit(const char *msg);
+int sameUser(char user);
+void createUser(char user);
 
-struct ThreadArgs {
-  int clntSock; // Socket descriptor for client
+struct ThreadArgs
+{
+  int clientSock; // Socket descriptor for client
 };
 
-int main(int argc, char *argv[]){
+struct userData
+{
+  int avail;
+  char nickName[20];
+  struct sockaddr_in addr;
+};
 
+int main(int argc, char *argv[])
+{
 
-    int retval;
-    int listenSock = socket(PF_INET, SOCK_DGRAM, 0);
+  int retval;
+  int listenSock = socket(PF_INET, SOCK_DGRAM, 0);
 
-    if(listenSock < 0){
-         err_quit("socket()");
-    }
+  if (listenSock < 0)
+  {
+    err_quit("socket()");
+  }
 
-    // Server address initialize and binding
-    struct sockaddr_in serveraddr;
-    memset(&serveraddr, 0, sizeof(serveraddr));
-    serveraddr.sin_family = AF_INET;
-    serveraddr.sin_port = htons(9000);
-    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  // Server address initialize and binding
+  struct sockaddr_in serveraddr;
+  memset(&serveraddr, 0, sizeof(serveraddr));
+  serveraddr.sin_family = AF_INET;
+  serveraddr.sin_port = htons(9000);
+  serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    retval = bind(listenSock, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
-    if (retval < 0) err_quit("bind()");
+  retval = bind(listenSock, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
+  if (retval < 0)
+    err_quit("bind()");
 
-    for(;;){
+  for (;;)
+  {
 
-    }
-    return 0;
+    struct ThreadArgs *threadArgs = (struct ThreadArgs *)malloc(
+        sizeof(struct ThreadArgs));
+
+    pthread_t mainThread;
+    int mainReturn = pthread_create(&mainThread, NULL, ThreadMain, threadArgs);
+  }
+  return 0;
 }
 
-
-void *ThreadMain(void *threadArgs) {
+void *ThreadMain(void *threadArgs)
+{
   // Guarantees that thread resources are deallocated upon return
   pthread_detach(pthread_self());
 
   // Extract socket file descriptor from argument
-  int clntSock = ((struct ThreadArgs *) threadArgs)->clntSock;
-  free(threadArgs); // Deallocate memory for argument
+  int clientSock = ((struct ThreadArgs *)threadArgs)->clientSock;
 
-  HandleTCPClient(clntSock);
+  pthread_t sendThread;
+  pthread_t recvThread; // Create client thread for send
+  int sendReturn = pthread_create(&sendThread, NULL, send, threadArgs);
+  int recvReturn = pthread_create(&recvThread, NULL, recv, threadArgs);
+
+  // free(threadArgs); // Deallocate memory for argument 양 스레드 끝나는 시점에 free 필요, 혹은 양 스레드 내부에서 처리
 
   return (NULL);
 }
 
+void *send(void *arg)
+{
+  pthread_detach(pthread_self());
 
-void err_quit(const char *msg) {
-    perror(msg);  // 시스템 오류 메시지 출력
-    exit(-1);     // 프로그램 종료
+  int sock = socket(AF_INET, SOCK_DGRAM, 0);
+  if (sock < 0)
+  {
+    err_quit("socket()");
+  }
+
+  while (1)
+  { // broadcast
+    for (int i = 0; sizeof(userList); i++)
+    {
+
+    }
+  }
 }
+
+void *recv(void *arg)
+{
+  pthread_detach(pthread_self());
+
+  int recv;
+  int nameLength = 0;
+  int sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+  while (1)
+  {
+
+    int addrlen = sizeof(clientaddr);
+
+    recv = recvfrom(sock, mainBuf, BUFSIZE, 0, (struct sockaddr *)&clientaddr, &addrlen);
+    if (recv >= 0)
+    {
+      messageNumber += 1;
+    }
+    if (recv < 0)
+    {
+      perror("recvfrom()");
+      continue;
+    }
+
+    for (int i = 1; mainBuf[i] == ']'; i++)
+    {
+      nameLength += 1;
+      nickNameBuf[i] = mainBuf[i];
+    }
+    if(sameUser(nickNameBuf)!=1){ // 새로운 유저라면
+      userCount += 1; // 유저 수 하나 늘리고
+      createUser(nickNameBuf); // 유저 DB에 정보 저장(닉네임, IP주소, 포트 번호)
+    }
+
+    char messageBuf[BUFSIZE - nameLength];
+    memcpy(&messageBuf, mainBuf, nameLength + 1);
+  }
+}
+
+void err_quit(const char *msg)
+{
+  perror(msg); // 시스템 오류 메시지 출력
+  exit(-1);    // 프로그램 종료
+}
+
+int sameUser(char user) // 데이터베이스에 존재하는 유저인지 체크
+{
+  for (int i = 0; sizeof(userList); i++)
+  {
+    if(strcmp(userList[i].nickName, user)==0){
+      return 1;
+    } else {
+      return -1;
+    }
+  }
+}
+void createUser(char user){
+
+}
+
+// gcc -o server UDPLiveServer.c
+// ./server
